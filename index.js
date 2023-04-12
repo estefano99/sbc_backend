ae4fe0c5cf85d515ffae7b77144628a1e58700a8
JuliBosch <julibosch99@gmail.com>
2023-04-12 16:45:10 -0300

    deploy del backend

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import conectarDB from './config/db.js';
+import socioRoutes from './routes/socioRoutes.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+conectarDB();
+
+//Configuracion para Cors
+const dominiosPermitidos = ["http://127.0.0.1:5173", "http://localhost:5173"];
+const corsOptions = {
+  origin: (origin,callback) => {
+    if (dominiosPermitidos.indexOf(origin) !== -1) {
+      callback(null,true)
+    }else{
+      callback(new Error("No permitido por cors"))
+    }
+  }
+}
+app.use(cors(corsOptions));
+// app.use(cors({ origin: '*' }));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use("/", socioRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcando en el puerto http://localhost:${PORT}`);
+});
